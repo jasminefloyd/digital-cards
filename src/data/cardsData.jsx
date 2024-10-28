698085d3fc18b070c1e6f2ec961c735eec86021d
jasminefloyd <42697356+jasminefloyd@users.noreply.github.com>
2024-10-28 19:35:28 -0400

    initial upload

---
+++ src/data/cardsData.jsx
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: 1,
+        cardImg: "me-ph.png",
+        name: "Jasmine Floyd",
+        title: "Full Stack Product Innovator",
+        website: "jasminefloyd.website",
+        emailBTN: "me@me.com",
+        linkedinBTN: "https://linkedin.com",
+        about: "I am Product Manager with a particular interest in making amazing things and constantly striving for greatness. I try to keep up with Product Management skills and best practices, and am always looking for new things to learn.",
+        interest: "Food expert. Reader. Entrepreneur. Travel geek. Coffee fanatic. Proud Member of the Laker Nation. Music scholar.",
+        qrlink: "https://link.com",
+        qrtext: "Scan QR Code to Save Contact Card",
+        facebook: "",
+        twitter: "",
+        instagram: "",
+        github: "",
+        linkedin: "",
+
+    },
+]
